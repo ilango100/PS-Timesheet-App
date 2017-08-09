0c4d61540bbcc72b3e6d6e06ec3c3eafd75c39eb
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-10 00:56:42 +0530

    HttpClient imported

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'login',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
 export class Login {
     user: string;
     pass: string;
+
+    constructor(private http: HttpClient) {
+    }
+
+    submit() {
+        //Send user pass data
+    };
 }
 
