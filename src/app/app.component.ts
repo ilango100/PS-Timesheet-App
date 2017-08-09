f562764a12f1e05bc1efdf6ee993edea482994d2
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-09 13:44:55 +0530

    selector renamed to ps-ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'ps-ts',
   template: `<h1>Hello {{name}}</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
