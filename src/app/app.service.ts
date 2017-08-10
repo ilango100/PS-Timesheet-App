65e86835ad62452ccd0995b46625b7710b66a70e
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-10 20:54:22 +0530

    Comment operations in service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,5 +4,14 @@ import { Comment } from './comment';
 
 @Injectable()
 export class AppService {
+    comments: Comment[] = [];
+
+    getComments():Comment[] {
+        return this.comments;
+    }
+
+    addComment(com:Comment) {
+        this.comments.push(com);
+    }
 
 }
