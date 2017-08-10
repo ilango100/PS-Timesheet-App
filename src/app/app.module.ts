f5b2e416d7b106fa559c2e74d499ef18383cc86d
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-10 17:54:51 +0530

    Breakform and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { Login } from './login.component';
+import { LoginComponent } from './login.component';
+import { BreakComponent } from './break.component';
+
+const routs: Routes = [
+  {
+    path: '',
+    redirectTo: '/break',
+    pathMatch: 'full',
+  },
+  {
+    path: 'break',
+    component: BreakComponent,
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, Login ],
+  imports:      [
+    RouterModule.forRoot(routs),
+    BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent, LoginComponent, BreakComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
     templateUrl: 'login.component.html',
 })
 
-export class Login {
+export class LoginComponent {
     user: string;
     pass: string;
     register = false;
