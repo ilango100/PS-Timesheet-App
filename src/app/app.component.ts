98c7efb14c1c955e7a09bb36a9e076bbcb3a344b
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-10 20:39:37 +0530

    Set login/logout time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,20 @@ export class AppComponent  {
   loggedIn = false;
   username: string;
 
+  loginTime: Date;
+  logoutTime: Date;
+
   logIn(log: boolean) {
     this.loggedIn = log;
     console.log("logged",log);
+    if (log) {
+      this.loginTime=new Date();
+    }
   };
 
   logOut() {
     console.log("logged out");
+    this.logoutTime = new Date();
   }
 
 }
