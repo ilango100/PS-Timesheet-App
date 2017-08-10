024d9e9aa77d44914b2e03928533d21abf5b654c
Ilango Rajagopal <ilangokul@gmail.com>
2017-08-10 20:47:22 +0530

    App service - performs all backgroud works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent }  from './app.component';
 import { LoginComponent } from './login.component';
 import { BreakComponent } from './break.component';
 import { DetailsComponent } from './details.component';
+import { AppService } from './app.service';
 
 const routs: Routes = [
   {
@@ -30,6 +31,7 @@ const routs: Routes = [
     RouterModule.forRoot(routs),
     BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, LoginComponent, BreakComponent, DetailsComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ AppService ]
 })
 export class AppModule { }
---
+++ src/app/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+import { Comment } from './comment';
+
+@Injectable()
+export class AppService {
+
+}
